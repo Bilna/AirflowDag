e5407c996b088ad55a3e345aeca04f5e06162338
Bilna P <bilna.p@gmail.com>
2022-05-09 15:53:34 +0530

    Update postgres_connect.py

--- tests/dags/postgres_connect.py
+++ tests/dags/postgres_connect.py
@@ -16,8 +16,3 @@ with DAG(
     postgres_conn_id="postgres_default",
     sql="sql/pet_schema.sql",
 )
-populate_pet_table = PostgresOperator(
-    task_id="populate_pet_table",
-    postgres_conn_id="postgres_default",
-    sql="sql/pet_schema.sql",
-)
