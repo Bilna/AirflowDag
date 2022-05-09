5004d3db31f50581c24179cd7859fb6a3796f48f
Bilna P <bilna.p@gmail.com>
2022-05-09 16:07:10 +0530

    Update postgres_connect.py

--- tests/dags/postgres_connect.py
+++ tests/dags/postgres_connect.py
@@ -1,18 +1,45 @@
 from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperatorimport airflow
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.postgres_operator import PostgresOperator
+from airflow.utils.dates import days_ago
 
-# create_pet_table, populate_pet_table, get_all_pets, and get_birth_date are examples of tasks created by
-# instantiating the Postgres Operator
+args={'owner': 'airflow'}
 
-with DAG(
-    dag_id="postgres_operator_dag",
-    start_date=datetime(2020, 2, 2),
-    schedule_interval="None",
-    catchup=False,
-) as dag:
-    create_pet_table = PostgresOperator(
-    task_id="create_pet_table",
-    postgres_conn_id="postgres_default",
-    sql="sql/pet_schema.sql",
+default_args = {
+    'owner': 'airflow',    
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+dag_psql = DAG(
+    dag_id = "postgresoperator_demo",
+    default_args=args,
+    # schedule_interval='0 0 * * *',
+    schedule_interval='None',	
+    dagrun_timeout=timedelta(minutes=60),
+    description='use case of psql operator in airflow',
+    start_date = airflow.utils.dates.days_ago(1)
 )
+create_table_sql_query = """CREATE TABLE employee (id INT NOT NULL, name VARCHAR(250) NOT NULL, dept VARCHAR(250) NOT NULL);"""
+insert_data_sql_query = """insert into employee (id, name, dept) values(1, 'vamshi','bigdata'),(2, 'divya','bigdata'),(3, 'binny','projectmanager'),(4, 'omair','projectmanager') ;"""
+
+create_table = PostgresOperator(
+    sql = create_table_sql_query,
+    task_id = "create_table_task",
+    postgres_conn_id = "postgres_local",
+    dag = dag_psql
+    )
+
+insert_data = PostgresOperator(
+    sql = insert_data_sql_query,
+    task_id = "insert_data_task",
+    postgres_conn_id = "postgres_local",
+    dag = dag_psql
+    )
+
+create_table >> insert_data
+
+    if __name__ == "__main__":
+        dag_psql.cli()
